84f1345b431944ebf8fcbdc0f369111225d50bb1
unknown <dmello.shelton@yahoo.com>
2021-12-11 11:27:46 -0500

    added product details screen using product mock data

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/products/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const Rating = ({ value, text, color }) => {
   return (
-    <div clasName="rating">
+    <div className="rating">
       <span>
         <i
           style={{ color: color }}
