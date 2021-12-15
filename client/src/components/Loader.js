8c49c24eeb6afc96c385ef903f122d1e0438ed66
unknown <dmello.shelton@yahoo.com>
2021-12-15 10:35:26 -0500

    Implemented redux, changed Products and Product Details to redux state and added Message and Loader components

---
+++ client/src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Spinner } from 'react-bootstrap'
+
+const Loader = ({ text }) => {
+    return (
+        <Spinner animation='border' role='status' style={{width: '100px', height: '100px', margin: 'auto', display: 'block'}}>
+            <span class='sr-only'>Loading {text}...</span>
+        </Spinner>
+    )
+}
+
+export default Loader
