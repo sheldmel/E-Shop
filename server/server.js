dc001428f778aa40356999d70919c6e376a1cc1e
unknown <dmello.shelton@yahoo.com>
2021-12-29 10:26:06 -0500

    prepared app for deployment to heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from "express";
-import path from 'path'
+import path, { dirname } from "path";
 import dotenv from "dotenv";
-import color from 'colors'
+import color from "colors";
 import connectDB from "./db/db.js";
-import morgan from 'morgan'
+import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errors.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -18,13 +18,9 @@ const app = express();
 
 app.use(express.json());
 
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -34,9 +30,21 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
-const __dirname = path.resolve()
 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 
